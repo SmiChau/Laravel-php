ca07c9a39f8957377ce9eba7b3808428f92a2bba
SmiChau <smichauxoxo@gmail.com>
2026-03-07 23:23:09 +0545

    feat:/ migration learning

--- database/migrations/2026_03_07_172924_create_categories_table.php
+++ database/migrations/2026_03_07_172924_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id('category_id');
             $table->string('category_name', 255);
+            $table->date("verified_at")->nullable();
             $table->timestamps();
         });
     }

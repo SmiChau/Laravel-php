9f3c64eb3cdd0b29ff380ff9fac48c8c28453364
SmiChau <smichauxoxo@gmail.com>
2026-03-08 15:42:39 +0545

    feat:/ Tailwand CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,9 @@
     <title>Document</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-gray-100 h-screen flex items-center justify-center">
-    <h1 class="text-3xl text-red-600">Hello World. This is Home Page</h1>
-
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded-xl shadow-md w-full max-w-xl text-center">
+        <h1 class="text-3xl text-red-600">Hello World. This is Home Page</h1>
+    </div>
 </body>
 </html>
